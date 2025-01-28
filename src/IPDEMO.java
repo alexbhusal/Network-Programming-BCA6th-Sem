a628159bcf97b0429f3bdc7ab6551b0b059062a8
Alex Bhusal <alexbhusal@icloud.com>
2025-01-28 19:16:53 +0545

    np

--- src/IPDEMO.java
+++ src/IPDEMO.java
@@ -4,6 +4,7 @@ import java.net.UnknownHostException;
 public class IPDEMO {
     public static void main(String[] args) throws UnknownHostException {
 
+//        To get IP, CNAME of the given Host
         String Hname = "bhuvanbhusal.com.np";
         InetAddress iName= InetAddress.getByName(Hname);
         String hostname = iName.getHostName();
@@ -13,10 +14,11 @@ public class IPDEMO {
         System.out.println("The Ip Address is: "+ip);
         System.out.println("The CNAME is: "+Cname+"\n");
 
+        // TO get the machine LocalHost IP
         InetAddress lAddress = InetAddress.getLocalHost();
         System.out.println("The LocalHost Address is: "+lAddress);
 
-
+        // TO check the given IP is IPV4 or Ipv6
         byte[] hostAddress =lAddress.getAddress();
         if(hostAddress.length == 6) {
             System.out.println("IP address is IPv6 address");
