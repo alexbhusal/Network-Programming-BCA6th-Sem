154fa1b672517c26f604fd3236630771f356d460
Alex Bhusal <alexbhusal@icloud.com>
2025-01-29 09:06:08 +0545

    np

--- src/netIterface.java
+++ src/netIterface.java
@@ -1,21 +1,26 @@
+import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
 public class netIterface {
-    public static void main(String[] args) throws SocketException {
+    public static void main(String[] args) throws SocketException, UnknownHostException {
 
         Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
         while (netInterfaces.hasMoreElements()) {
             NetworkInterface netInterface = netInterfaces.nextElement();
             System.out.println(netInterface);
         }
+        System.out.println("\n");
 
-        NetworkInterface nt= NetworkInterface.getByName("en0");
-        if(nt == null) {
-            System.out.println("No network interface found.");
+        String Host = "127.0.0.1";
+        InetAddress local= InetAddress.getByName(Host);
+        NetworkInterface netInterface = NetworkInterface.getByInetAddress(local);
+        if(netInterface==null){
+            System.out.println("No network interface found");
         }else{
-            System.out.println("Network interface found with en0");
+            System.out.println("Network interface found with name "+netInterface.getName());
         }
 
     }
