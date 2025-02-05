04ba518cdd1467e44a038145bd033845c8edeb0c
Alex Bhusal <alexbhusal@icloud.com>
2025-02-05 09:03:12 +0545

    UNIT-3

---
+++ src/UrlandUri.java
@@ -0,0 +1,17 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UrlandUri {
+    public static void main(String[] args) throws MalformedURLException {
+        URL url = new URL("https://alexbhusal.com:8080/mac/book.html?query=alexbhusal#12");
+        System.out.println("URL: " + url.toString());
+        System.out.println("protocol: " + url.getProtocol());
+        System.out.println("host: " + url.getHost());
+        System.out.println("port: " + url.getPort());
+        System.out.println("file: " + url.getFile());
+        System.out.println("path: " + url.getPath());
+        System.out.println("query: " + url.getQuery());
+        System.out.println("referer: " + url.getRef());
+        System.out.println("Default port: " + url.getDefaultPort());
+    }
+}
